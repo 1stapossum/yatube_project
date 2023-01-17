a0c35ca4383a839414d23d08cc24c604abeb37ba
Alexander V. Volokhov <me@avolokhov.ru>
2023-01-17 18:16:35 +0300

    Регистрация модели Post в админке

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
-    path('group_list/', include('posts.urls', namespace='posts')),
+#    path('group_list/', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls)
     ]
